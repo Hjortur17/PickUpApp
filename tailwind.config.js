0c826a5da9560e2a5fc630ded54afc4044228fc4
Hjörtur Freyr <hjorturfreyr@hjorturfreyr.com>
2022-12-20 16:27:43 +0000

    Initial commit | Landing page ready

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */;
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+        colors: {
+            neutral: {
+                50: "#F7F7F7",
+                100: "#E1E1E1",
+                200: "#CFCFCF",
+                300: "#B1B1B1",
+                400: "#9E9E9E",
+                500: "#7E7E7E",
+                600: "#626262",
+                700: "#515151",
+                800: "#3B3B3B",
+                900: "#222222",
+            },
+        },
+    },
+},
+  plugins: [],
+}

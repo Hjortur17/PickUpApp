679bc8486dc7769826dd6b580c7b7964a30ead2a
Hjörtur Freyr <hjorturfreyr@hjorturfreyr.com>
2022-12-21 10:40:11 +0000

    Fixed size - French navbar

--- screens/fr/CarLocated.jsx
+++ screens/fr/CarLocated.jsx
@@ -22,7 +22,7 @@ export default function FrenchCarLocated({navigation}) {
 
     return (
         <StyledView id='container' className="max-w-screen-lg w-full mx-auto relative">
-            <Navbar text='Retourner' navigation={navigation} size='32' />
+            <Navbar text='Retourner' navigation={navigation} size='36' />
 
             <StyledView className='h-full flex justify-center items-center space-y-3 max-w-screen-md mx-auto'>
                 <StyledBoldTitle style={{fontFamily: 'LibreFranklin_800ExtraBold'}} className='text-left'>
